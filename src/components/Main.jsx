c9d109629c7a83a4b8dc5d02b993afe7b1fffca5
Ark-Alvarenga <82475496+Ark-Alvarenga@users.noreply.github.com>
2024-10-09 15:32:40 -0300

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ const Main = (props) => {
   const { data } = props;
   return (
     <div className="imgContainer">
-      <img src={data.hdurl} alt={data.title || "bg-img"} className="bgImage" />;
+      <img src={data.hdurl} alt={data.title || "bg-img"} className="bgImage" />
     </div>
   );
 };
